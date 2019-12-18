e3a85da62f4664dfb2ff3494eb78ae3ef1a602f6
Lon Blauvelt <lblauvel@ucsc.edu>
2019-12-18 12:29:46 -0800

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -1,8 +1,3 @@
-import os
-
-from uuid import uuid4
-from create_project import run
-
 """
 Running this will parse all excel files into bundles output into the folder: 'testing_comparison'.
 
@@ -18,6 +13,10 @@ also have slightly different formatting (all seem to be missing a "funders" sect
 Editing 'upload=False' to 'upload=True' with upload to dss dev if you are credentialed to do so.
 Otherwise it will just parse the excel files and generate all of the matrix and json files necessary for upload.
 """
+import os
+
+from create_project import run
+
 
 """6 ORIGINAL DATASETS (ALREADY IN THE DSS)"""
 # These are the original excel files provided that currently exist in dss prod
@@ -39,8 +38,9 @@ for project in range(6):
 src_dir = 'raw_excel_inputs'
 projects = [f for f in os.listdir(src_dir) if os.path.isfile(os.path.join(src_dir, f)) and f.endswith('.0.xlsx')]
 
-for project in projects:
-    print(f'\nProject: {project}')
+for i, project in enumerate(projects):
+    print(f'\n% Progress: {i + 1}/{len(projects)} projects ({project}).\n'
+          f'===========================================================')
     run(xlsx=f'raw_excel_inputs/{project}',
         output_dir=f'testing_comparison/{project[:-5]}',
         upload=False)
