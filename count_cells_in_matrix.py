5530b5fc428f29e2ce2f1f67f7094fb83fc013bf
Daniel Sotirhos <dsotirho@ucsc.edu>
2020-01-02 10:18:41 -0800

    fixup! Save cell counts in global cell_counts.json file

--- count_cells_in_matrix.py
+++ count_cells_in_matrix.py
@@ -4,7 +4,10 @@ import json
 import os
 import sys
 
-from typing import Sequence
+from typing import (
+    Sequence,
+    Union
+)
 
 import logging
 logging.basicConfig(level=logging.INFO)
@@ -14,17 +17,17 @@ def main(argv):
     parser = argparse.ArgumentParser(description=__doc__)
     group = parser.add_mutually_exclusive_group(required=True)
     group.add_argument('--list', '-l',
-                       metavar='MATRIX_FILE',
-                       help='list cell count value for given matrix file')
+                       metavar='ACCESSION',
+                       help='list cell count value for given accession id')
     group.add_argument('--write', '-w',
-                       metavar='MATRIX_FILE',
-                       help='write a cell count file for given matrix file')
+                       metavar='ACCESSION',
+                       help='write a cell count file for given accession id')
     group.add_argument('--list-all', '-L',
                        action='store_true',
-                       help='list cell counts for all project matrix files')
+                       help='list cell counts for all accession ids')
     group.add_argument('--write-all', '-W',
                        action='store_true',
-                       help='write cell count files for all project matrix files')
+                       help='write cell count files for all accession ids')
     parser.add_argument('--verbose', '-v',
                         action='store_true',
                         help='Verbose debug output')
@@ -47,45 +50,47 @@ def count_all_project_cells(save_to_file=True):
     """
     Count cells in all projects
 
-    :param save_to_file: If true write each cell count to a file in the project's folder
+    :param save_to_file: If true write the cell count totals to a global cell count file
     """
-    for project_path in get_project_paths():
-        logging.info('Checking: %s ...', project_path)
-        count_one_project_cells(project_path, save_to_file)
+    for accession_id in get_accession_ids():
+        logging.info('Checking: %s ...', accession_id)
+        count_one_project_cells(accession_id, save_to_file)
 
 
-def count_one_project_cells(path: str, save_to_file=True):
+def count_one_project_cells(accession_id: str, save_to_file=True):
     """
     Count cells in one project
 
-    :param path: A path to a project folder or matrix file
-    :param save_to_file: If true write the cell count to a file in the project's folder
+    :param accession_id: An accession id with a downloaded matrix file
+    :param save_to_file: If true write the cell count total(s) to a global cell count file
     """
-    cell_count = get_project_cell_count(path)
+    cell_count = get_project_cell_count(accession_id)
     if save_to_file:
-        if os.path.isdir(path):
-            project_path = path
-        else:
-            project_path = '/'.join(path.split('/')[:-1])
-        accession_id = accession_id_from_project_path(project_path)
         update_cell_count_file(accession_id, cell_count)
 
 
-def get_project_paths() -> Sequence[str]:
+def get_accession_ids() -> Sequence[str]:
     """
-    Return a list of the project folder paths
+    Return a list of the accession ids
     """
-    project_paths = []
+    accession_ids = []
     for item in os.listdir('projects'):
-        project_path = f'projects/{item}'
-        logging.debug('Checking: %s', project_path)
-        if os.path.isdir(project_path) and not os.path.islink(project_path):
-            project_paths.append(project_path)
-    return project_paths
+        path = f'projects/{item}'
+        logging.debug('Checking: %s', path)
+        if os.path.isdir(path) and os.path.islink(path):
+            accession_ids.append(item)
+    return accession_ids
 
 
 def update_cell_count_file(accession_id: str, cell_count: int) -> bool:
-    if not accession_id or cell_count is None:
+    """
+    Write the accession cell count to the global cell count file
+
+    :param accession_id: An accession id
+    :param cell_count: An int value to save, or None to remove entry
+    :return: Boolean status of the save
+    """
+    if not accession_id:
         return False
     cell_counts_file = 'cell_counts.json'
     if os.path.exists(cell_counts_file):
@@ -93,55 +98,34 @@ def update_cell_count_file(accession_id: str, cell_count: int) -> bool:
             cell_counts = json.loads(f.read())
     else:
         cell_counts = {}
-    logging.info('Writing to cell counts json: accession=%s count=%s', accession_id, cell_count)
-    cell_counts[accession_id] = cell_count
+    if isinstance(cell_count, int):
+        logging.info('Writing accession %s cell count.', accession_id)
+        cell_counts[accession_id] = cell_count
+    elif cell_count is None and accession_id in cell_counts:
+        logging.info('Removing accession %s cell count.', accession_id)
+        del(cell_counts[accession_id])
     with open(cell_counts_file, 'w') as f:
         f.write(json.dumps(cell_counts, sort_keys=True, indent='    '))
     return True
 
 
-def accession_id_from_project_path(project_path: str) -> str:
-    # Try to get the accession id from the project json file
-    # TODO: handle multiple accession ids in project json
-    project_json = f'{project_path}/bundle/project_0.json'
-    if os.path.exists(project_json):
-        with open(project_json, 'r') as f:
-            accession_id = ''.join(json.loads(f.read()).get('geo_series_accessions', []))
-            logging.debug('Found accession id "%s" in project json', accession_id)
-            return accession_id
-    # Try to get the accession id from the filename of the downloaded geo files
-    # TODO: handle multiple accession ids in filenames
-    geo_dir = f'{project_path}/geo'
-    if os.path.isdir(geo_dir):
-        for filename in os.listdir(geo_dir):
-            if filename.startswith('GSE') and '_' in filename:
-                accession_id = filename[:filename.index('_')]
-                logging.debug('Found accession id "%s" from filename %s', accession_id, filename)
-                return accession_id
-    return None
-
-
-def get_project_cell_count(path: str) -> int:
+def get_project_cell_count(accession_id: str) -> Union[int, None]:
     """
     Get the cell count from a project's matrix file
 
-    :param path: A path to a project folder or matrix file
+    :param accession_id: An accession id that has a downloaded matrix file
     :return: A count of cells
     """
-    matrix_file_full = None
-    if os.path.isfile(path):
-        matrix_file_full = path
-    elif os.path.isdir(path):
-        matrix_file_full = f'{path}/matrix.mtx'
-    if not matrix_file_full or not os.path.exists(matrix_file_full):
-        logging.debug('Unable to find matrix file at path: %s', path)
+    matrix_file_full = f'projects/{accession_id}/matrix.mtx'
+    if not os.path.isdir(f'projects/{accession_id}') or not os.path.isfile(matrix_file_full):
+        logging.warning('Unable to find matrix file %s', matrix_file_full)
         return None
     cell_count = count_cells(matrix_file_full)
-    logging.info('Cell count in %s: %s', path, cell_count)
+    logging.info('Cell count in %s is %s', accession_id, cell_count)
     return cell_count
 
 
-def count_cells(matrix_file: str) -> int:
+def count_cells(matrix_file: str) -> Union[int, None]:
     """
     Count the number of cells in a matrix file
 
@@ -151,15 +135,9 @@ def count_cells(matrix_file: str) -> int:
     if not os.path.exists(matrix_file):
         logging.error(f'File not found "{matrix_file}"')
         return None
-    barcode_indexes = set(())
+    barcode_indexes = set()
     with open(matrix_file, newline='') as csv_file:
-        csv_reader = None
-        try:
-            dialect = csv.Sniffer().sniff(csv_file.read(1024))
-        except csv.Error:
-            pass  # Retry one more time if first read couldn't be sniffed
-        if not csv_reader:
-            dialect = csv.Sniffer().sniff(csv_file.read(1024))
+        dialect = csv.Sniffer().sniff(csv_file.read(2048))
         csv_file.seek(0)
         csv_reader = csv.reader(csv_file, dialect)
         next(csv_reader)  # skip header row
