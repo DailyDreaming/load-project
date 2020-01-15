DailyDreaming/load-project

commit 42a578ac4e0e5fb14088d58b787f160a5575fa89

Refactor CSV idempotence

convert_matrices.py
@@ -107,24 +107,21 @@ class Converter(metaclass=ABCMeta):
                     idempotent_link(self.geo_dir / src_name, dst_dir / dst_name)
 
     def _convert_csvs(self, *csvs: CSV):
+        expected_files = {'matrix.mtx.gz', 'genes.tsv', 'barcodes.tsv'}
         for csv in csvs:
-            self.idempotent_csv_conversion(csv)
-
-    def idempotent_csv_conversion(self, csv):
-        expected_files = [f for f in ('matrix.mtx.gz', 'genes.tsv', 'barcodes.tsv')
-                          if (self.matrix_dir(csv.name) / f).exists()]
-        if len(expected_files) == 3:
-            log.info('Matrix already generated for CSV `%s`', csv.name)
-        else:
-            if len(expected_files) == 0:
-                log.info('Started CSV conversion for CSV `%s`', csv.name)
+            output_dir = self.matrix_dir(csv.name)
+            actual_files = {f for f in expected_files if (output_dir / f).exists()}
+            if actual_files == expected_files:
+                log.info('Matrix already generated for CSV `%s`', csv.name)
             else:
-                log.warning('Only found the following expected files %s', expected_files)
-            convert_csv_to_mtx(input_file=self.geo_dir / csv.name,
-                               output_dir=(self.matrix_dir(csv.name)),
-                               delimiter=csv.sep,
-                               rows_are_genes=csv.rows_are_genes,
-                               row_filter=csv.row_filter)
+                if actual_files:
+                    log.warning('Found partial conversion results. Missing files: %s', expected_files - actual_files)
+                log.info('Started CSV conversion for `%s`', csv.name)
+                convert_csv_to_mtx(input_file=self.geo_dir / csv.name,
+                                   output_dir=output_dir,
+                                   delimiter=csv.sep,
+                                   rows_are_genes=csv.rows_are_genes,
+                                   row_filter=csv.row_filter)
 
     def _fix_short_rows(self, row_length: int) -> RowFilter:
         """

commit 43237968059e9a549cba8f0a5a881ccb7a3e26f4

Fix mtx idempotence
Regression from 42a578ac4e0e5fb14088d58b787f160a5575fa89

convert_matrices.py
@@ -125,7 +125,7 @@ class Converter(metaclass=ABCMeta):
                     idempotent_link(self.geo_dir / src_name, dst_dir / dst_name)
 
     def _convert_matrices(self, *inputs: Union[CSV, H5]):
-        expected_files = {'matrix.mtx.gz', 'genes.tsv', 'barcodes.tsv'}
+        expected_files = {'matrix.mtx.gz', 'genes.tsv.gz', 'barcodes.tsv.gz'}
         for input in inputs:
             output_dir = self.matrix_dir(input.name)
             actual_files = {f for f in expected_files if (output_dir / f).exists()}