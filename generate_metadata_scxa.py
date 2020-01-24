7c99b3b9e1fd126b5ee36fe0bb651d5b36eef0d7
jessebrennan <brennan@ucsc.edu>
2020-01-24 15:50:20 -0800

    Generate metadata for SCXA projects

--- generate_metadata_scxa.py
+++ generate_metadata_scxa.py
@@ -1,11 +1,25 @@
+import json
+import logging
 from typing import List
 
 from _pathlib import Path
 
 from more_itertools import one
 
-from create_project import timestamp, write_project_json
-from util import get_target_project_dirs, generate_project_uuid
+from count_cells import CountCells
+from create_project import (
+    timestamp,
+    write_project_json,
+    generate_analysis_json,
+    generate_links_json,
+)
+from util import (
+    get_target_project_dirs,
+    generate_project_uuid,
+    generate_file_uuid,
+)
+
+log = logging.getLogger(__name__)
 
 
 def main():
@@ -16,16 +30,60 @@ def main():
 
 
 def generate_metadata(project_dir: Path):
-    project_json = create_project_json(project_dir)
-    write_project_json(project_json, bundle_dir(project_dir))
+    accession = accession_from_project_dir(project_dir)
+    project_uuid = generate_project_uuid(accession)
+    output_dir = bundle_dir(project_dir)
+
+    project_json = create_project_json(accession, project_dir, project_uuid)
+    write_project_json(project_json, output_dir)
+    generate_analysis_json(project_uuid, output_dir)
+
+    cell_count = CountCells.get_cached_cell_count(project_dir)
+    generate_cell_suspension_json(output_dir, cell_count, project_uuid)
+
+    generate_links_json(output_dir)
 
 
 def bundle_dir(project_dir: Path):
     return project_dir / 'bundle'
 
 
-def create_project_json(project_dir: Path):
-    accession = accession_from_project_dir(project_dir)
+def generate_cell_suspension_json(output_dir, cell_count, bundle_uuid):
+    file_name = 'cell_suspension_0.json'
+    cell_json = create_cell_suspension_jsons(cell_count, generate_file_uuid(bundle_uuid, file_name))
+    with open(f'{output_dir}/{file_name}', 'w') as f:
+        f.write(json.dumps(cell_json, indent=4))
+    print(f'"{output_dir}/{file_name}" successfully written.')
+
+
+def create_cell_suspension_jsons(cell_count, file_uuid):
+    version = timestamp()
+    cell_suspension_json = {
+        "describedBy": "https://schema.dev.data.humancellatlas.org/type/biomaterial/13.1.0/cell_suspension",
+        "schema_type": "biomaterial",
+        "biomaterial_core": {
+            "biomaterial_id": str(file_uuid),
+            "biomaterial_name": None,
+            "biomaterial_description": None,
+            "ncbi_taxon_id": [0],
+            "genotype": None
+        }, "genus_species": [
+            {
+                "text": None,
+                "ontology": None,
+                "ontology_label": None
+            }
+        ], "provenance": {
+            "document_id": str(file_uuid),
+            "submission_date": version,
+            "update_date": version
+        },
+        "estimated_cell_count": cell_count,
+    }
+    return cell_suspension_json
+
+
+def create_project_json(accession: str, project_dir: Path, project_uuid):
     idf_file = file_by_suffix(scxa_dir(project_dir), 'idf.txt')
     idf_metadata = parse_mage_tab(idf_file)
     version = timestamp()
@@ -33,12 +91,14 @@ def create_project_json(project_dir: Path):
         "describedBy": "https://schema.dev.data.humancellatlas.org/type/project/14.0.0/project",
         "schema_type": "project",
         "project_core": {
-            "project_short_name": only(idf_metadata['Investigation Title']),
+            "project_short_name": accession,
             "project_title": only(idf_metadata['Investigation Title']),
-            "project_description": only(idf_metadata['Experiment Description']),
+            **({
+                "project_description": only(idf_metadata['Experiment Description']),
+            } if 'Experiment Description' in idf_metadata else {})
         },
         "provenance": {
-            "document_id": generate_project_uuid(accession),
+            "document_id": project_uuid,
             "submission_date": version,
             "update_date": version
         }
@@ -68,14 +128,15 @@ def only(row: List[str]):
 
 def parse_mage_tab(idf_file: Path):
     data = {}
+    log.debug('Parsing IDF file "%s"', idf_file)
     with idf_file.open('r') as f:
-        assert f.readline().startswith('MAGE-TAB Version')
         for line in f.readlines():
             line = line.rstrip('\n')
             cols = line.split('\t')
             if len(cols) > 0:
                 header, *values = cols
                 data[header] = values
+    assert 'MAGE-TAB Version' in data
     return data
 
 
@@ -86,3 +147,8 @@ def file_by_suffix(scxa: Path, suffix: str):
 
 def scxa_dir(project_dir):
     return project_dir / 'scxa'
+
+
+if __name__ == "__main__":
+    logging.basicConfig(format='%(asctime)s %(levelname)s %(threadName)s: %(message)s', level=logging.DEBUG)
+    main()
