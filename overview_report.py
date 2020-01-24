DailyDreaming/load-project

commit b70bc1da7d19784da2a6faebf3c8bdd4c9c24df6

Only download unfamiliar projects from SCXA

overview_report.py
@@ -140,9 +140,8 @@ def overview_report() -> Mapping[UUID, ProjectReport]:
 
     # ---
     logging.debug('Searching for spreadsheets ...')
-    for file in get_target_spreadsheets():
+    for accession_id, file in get_target_spreadsheets():
         logging.debug('Checking: %s', file)
-        accession_id = file.name[:-len('.0.xlsx')]
         uuid = UUID(generate_project_uuid(accession_id))
         try:
             report[uuid].spreadsheet = file

download.py
@@ -25,8 +25,7 @@ source_url_template = 'https://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc='
 
 
 def main():
-    accessions = [p.name.split('.')[0] for p in get_target_spreadsheets()]
-    for accession in accessions:
+    for accession in get_target_spreadsheets().keys():
         download_supplementary_files(accession)
 
 

download_scxa.py
@@ -10,7 +10,7 @@ from urllib.request import urlretrieve
 import requests
 
 from download import create_or_update_symlink, download_file
-from util import generate_project_uuid
+from util import generate_project_uuid, get_target_spreadsheets, get_skunk_accessions
 
 log = logging.getLogger(__name__)
 
@@ -23,13 +23,32 @@ def download_projects(path: Path):
 
 def all_accessions():
     projects = list_projects()
-    accessions = [p['experimentAccession'] for p in projects]
+    accessions = {p['experimentAccession'] for p in projects}
     return accessions
+
+
+def scxa_geo_accessions():
+    accessions = set()
+    for accession in (get_target_spreadsheets().keys()):
+        assert accession.startswith('GSE')
+        accessions.add('E-GEOD-' + accession[3:])
+    return accessions
+
+
+def accessions_to_download():
+    geo_accessions = scxa_geo_accessions()
+    hca_accessions = {'E-MTAB-5061', 'E-EHCA-2', 'E-MTAB-6701', 'E-ENAD-15'}
+    skunk_accessions = get_skunk_accessions() or set()
+    excluded_accessions = geo_accessions | hca_accessions | skunk_accessions
+    return {
+        accession for accession in all_accessions()
+        if accession not in excluded_accessions
+    }
 
 
 def download_projects_parallel(path: Path):
     with ThreadPoolExecutor() as executor:
-        for accession in all_accessions():
+        for accession in accessions_to_download():
             accessions_to_futures = defaultdict(set)
             scxa_path = make_and_link_download_dir(accession, path)
             files = project_files(accession)

generate_metadata.py
@@ -20,7 +20,7 @@ from create_project import (
 from util import get_target_spreadsheets
 
 
-xlsxs = get_target_spreadsheets()
+xlsxs = get_target_spreadsheets().values()
 
 for i, xlsx in enumerate(xlsxs):
     print(f'\n% Progress: {i + 1}/{len(xlsxs)} projects ({xlsx.name}).\n'

scxa.ipynb
@@ -429,7 +429,22 @@
    "source": [
     "from generate_metadata_scxa import parse_mage_tab, file_by_suffix, generate_metadata\n",
     "test_project = Path('projects') / test_accession\n",
-    "generate_metadata(test_project)"
+    "generate_metadata(test_project)\n"
+   ],
+   "metadata": {
+    "collapsed": false,
+    "pycharm": {
+     "name": "#%%\n",
+     "is_executing": false
+    }
+   }
+  },
+  {
+   "cell_type": "code",
+   "execution_count": 21,
+   "outputs": [],
+   "source": [
+    "hca_accessions = {d['experimentAccession'] for d in hca_projects}"
    ],
    "metadata": {
     "collapsed": false,

util.py
@@ -5,6 +5,7 @@ from typing import (
     Optional,
     Sequence,
     Union,
+    MutableMapping,
 )
 import uuid
 
@@ -65,23 +66,34 @@ def get_skunk_accessions() -> Optional[List[str]]:
         return [acc.strip() for acc in accessions.split(',') if acc.strip()]
 
 
-def get_target_spreadsheets() -> List[Path]:
+def get_target_spreadsheets() -> MutableMapping[str, Path]:
     accessions = get_skunk_accessions()
-    spreadsheet_paths = []
+    paths_by_accession = {}
     ext = '.0.xlsx'
+
+    def get_accession_from_path(path):
+        assert path.name.endswith(ext)
+        return path.name[:-len(ext)]
+
     for sub_dir in ('existing', 'new'):
         src_dir = Path('spreadsheets') / sub_dir
-        subdir_paths = [
+        paths = list(src_dir.iterdir())
+        paths = [
             path
-            for path
-            in src_dir.iterdir()
-            if (path.is_file()
-                and path.name.endswith(ext)
-                and (accessions is None
-                     or path.name.replace(ext, '') in accessions))
+            for path in paths
+            if path.is_file() and path.name.endswith(ext)
         ]
-        spreadsheet_paths.extend(subdir_paths)
-    return spreadsheet_paths
+        subdir_paths_by_accession = {
+            get_accession_from_path(path): path
+            for path in paths
+        }
+        assert len(paths) == len(subdir_paths_by_accession)
+        subdir_paths_by_accession = {
+            accession: path for accession, path in subdir_paths_by_accession.items()
+            if accessions is None or accession in accessions
+        }
+        paths_by_accession.update(subdir_paths_by_accession)
+    return paths_by_accession
 
 
 def get_target_project_dirs(follow_links: bool = False) -> List[Path]:

commit e6a7717a969382dababeb5ed3a46556bbe983c6f

Fix tuple assignment in overview_report.py
Regression from b70bc1da7d19784da2a6faebf3c8bdd4c9c24df6

overview_report.py
@@ -140,7 +140,7 @@ def overview_report() -> Mapping[UUID, ProjectReport]:
                                                                accession=accession_id)
 
     logging.debug('Searching for spreadsheets ...')
-    for accession_id, file in get_target_spreadsheets():
+    for accession_id, file in get_target_spreadsheets().items():
         logging.debug('Checking: %s', file)
         uuid = UUID(generate_project_uuid(accession_id))
         try: